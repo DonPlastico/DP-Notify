e8cf0bb587dd810ffce59fcda819523d013c46ab
Alkasana23 <alexneitor5@gmail.com>
2025-08-11 17:45:54 +0200

    Arreglo de fallos

--- cl_main.lua
+++ cl_main.lua
@@ -1,7 +1,12 @@
 RegisterCommand('testnotify', function()
     TriggerEvent('DP-Notify', 'Notificar prueba exitosa.', 'success', 10000)
+    Wait(2500)
     TriggerEvent('DP-Notify', 'Prueba Notificar error.', 'error', 10000)
+    Wait(2500)
     TriggerEvent('DP-Notify', 'Prueba Notificar información.', 10000)
+    Wait(2500)
+    TriggerEvent('DP-Notify', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus nibh nec augue pulvinar, eget cursus arcu gravida. Nulla facilisi. In ut urna eu dui condimentum lobortis id et leo. Suspendisse sed varius mauris. Etiam sit amet elit non libero gravida tempus placerat a purus. In interdum felis eleifend ligula varius molestie. Sed interdum nulla at eros rhoncus, in venenatis lorem ultricies. Mauris vitae libero non risus imperdiet sollicitudin. Fusce pulvinar, tortor et accumsan aliquam, justo erat dignissim odio, sit amet consequat neque arcu eu diam. Integer felis ligula, egestas sed sodales at, varius in eros. Suspendisse mollis ante nec nisl sodales euismod. Etiam non tellus dignissim, fermentum odio quis, tincidunt ipsum. Fusce cursus, leo eget laoreet mattis, dui orci commodo justo, vitae ullamcorper tortor enim nec metus. Cras fringilla, leo ac interdum ornare, nunc erat vehicula neque, non eleifend tellus nisl eget lectus. Etiam iaculis magna nulla, eu pellentesque sem venenatis a. Aliquam in laoreet ex, eu convallis tortor. Maecenas nec lorem nec odio vulputate accumsan. Sed et venenatis dolor. Nullam eu mauris nulla. Pellentesque porta tellus magna, ut elementum risus viverra et. Duis consectetur, nisl sit amet suscipit sollicitudin, nisl leo consectetur nisi, vel eleifend arcu purus in ligula.', 10000)
+    
 end)
 
 RegisterCommand('notifysettings', function()
